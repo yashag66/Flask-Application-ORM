c867d2ef23984d599b9c3a7f267f9a2e1d7cdeed
Yash Agarwal <yashag66@gmail.com>
2020-03-20 14:16:22 +0530

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -8,6 +8,7 @@ app = Flask(__name__, instance_relative_config=False)
 app.config.from_object('config.Config')
 login = LoginManager(app)
 login.login_view = 'login'
+login_manager.login_message = "Please login to View the content"
 db.init_app(app)
 
 
@@ -18,20 +19,3 @@ with app.app_context():
     # Create tables for our models
     db.create_all()
 
-
-
-# def create_app():
-#     """Construct the core application."""
-#     app = Flask(__name__, instance_relative_config=False)
-#     app.config.from_object('config.Config')
-#     login = LoginManager(app)
-#     db.init_app(app)
-#
-#     with app.app_context():
-#         # Imports
-#         from . import routes
-#
-#         # Create tables for our models
-#         db.create_all()
-#
-#         return app, login
